cd7d625d22f00cb681425944fa9f4f30717215ad
Mx444 <mybbtester@gmail.com>
2024-07-19 16:09:36 +0200

    fix: dirpath

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { createServer } from "http";
+import { createServer } from "node:http";
 import { createApp } from "./server";
 import { setupSocket } from "./socket";
 
+const port = process.env.PORT || 3000;
 const app = createApp();
 const server = createServer(app);
 const io = setupSocket(server);
 
-server.listen(3000, () => console.log(`Server is running http://localhost:${3000}`));
-app.listen(8080, () => console.log(`Server is running http://localhost:${8080}`));
+server.listen(port, () => console.log(`Server is running http://localhost:${port}`));
